317e233732b60abae41f7f8d88243632945804e4
Michael Tolly <miketolly@gmail.com>
2015-01-22 23:52:19 -0600

    encapsulated fns in object, enter button to submit login form

--- js/all.js
+++ js/all.js
@@ -1,90 +1,121 @@
 (function() {
-  var callAris, loadLogin, login, logout, selectPage;
+  var App, app;
 
   console.log('CoffeeScript loaded.');
 
   $.cookie.json = true;
 
-  callAris = function(func, json, cb) {
-    var req;
-    if (cb == null) {
-      cb = function(x) {
-        window.arisResult = x;
-        return console.log(x);
-      };
-    }
-    req = new XMLHttpRequest;
-    req.onreadystatechange = function() {
-      if (req.readyState === 4) {
-        if (req.status === 200) {
-          return cb(JSON.parse(req.responseText));
-        } else {
-          return cb(false);
-        }
-      }
-    };
-    req.open('POST', "http://dev.arisgames.org/server/json.php/v2." + func, true);
-    req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    return req.send(JSON.stringify(json));
-  };
-
-  loadLogin = function() {
-    return window.auth = $.cookie('auth');
-  };
-
-  login = function(username, password, cb) {
-    if (cb == null) {
-      cb = (function() {});
-    }
-    return callAris('users.logIn', {
-      user_name: username,
-      password: password,
-      permission: 'read_write'
-    }, function(_arg) {
-      var returnCode, user;
-      user = _arg.data, returnCode = _arg.returnCode;
-      if (returnCode === 0) {
-        window.auth = {
-          user_id: parseInt(user.user_id),
-          permission: 'read_write',
-          key: user.read_write_key
+  App = (function() {
+    function App() {
+      $(document).ready((function(_this) {
+        return function() {
+          $('#form-login').submit(function() {
+            _this.login($('#text-username').val(), $('#text-password').val(), function() {
+              if (_this.auth != null) {
+                return _this.selectPage('#page-list');
+              }
+            });
+            return false;
+          });
+          $('#button-logout').click(function() {
+            _this.logout();
+            return _this.selectPage('#page-login');
+          });
+          _this.loadLogin();
+          if (_this.auth != null) {
+            return _this.selectPage('#page-list');
+          } else {
+            return _this.selectPage('#page-login');
+          }
         };
-        $.cookie('auth', window.auth);
-      }
-      return cb();
-    });
-  };
-
-  logout = function() {
-    window.auth = null;
-    return $.removeCookie('auth');
-  };
-
-  selectPage = function(page) {
-    $('.page').addClass('page-hidden');
-    return $(page).removeClass('page-hidden');
-  };
-
-  $(document).ready(function() {
-    $('#button-login').click(function() {
-      return login($('#text-username').val(), $('#text-password').val(), function() {
-        if (window.auth != null) {
-          return selectPage('#page-list');
-        }
-      });
-    });
-    $('#button-logout').click(function() {
-      logout();
-      return selectPage('#page-login');
-    });
-    loadLogin();
-    if (window.auth != null) {
-      return selectPage('#page-list');
-    } else {
-      return selectPage('#page-login');
+      })(this));
     }
-  });
 
-  window.callAris = callAris;
+    App.prototype.callAris = function(func, json, cb) {
+      var req;
+      if (cb == null) {
+        cb = function(x) {
+          this.arisResult = x;
+          return console.log(x);
+        };
+      }
+      if (this.auth != null) {
+        json.auth = this.auth;
+      }
+      req = new XMLHttpRequest;
+      req.onreadystatechange = (function(_this) {
+        return function() {
+          if (req.readyState === 4) {
+            if (req.status === 200) {
+              return cb(JSON.parse(req.responseText));
+            } else {
+              return cb(false);
+            }
+          }
+        };
+      })(this);
+      req.open('POST', "http://dev.arisgames.org/server/json.php/v2." + func, true);
+      req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      return req.send(JSON.stringify(json));
+    };
+
+    App.prototype.loadLogin = function() {
+      return this.auth = $.cookie('auth');
+    };
+
+    App.prototype.login = function(username, password, cb) {
+      if (cb == null) {
+        cb = (function() {});
+      }
+      return this.callAris('users.logIn', {
+        user_name: username,
+        password: password,
+        permission: 'read_write'
+      }, (function(_this) {
+        return function(_arg) {
+          var returnCode, user;
+          user = _arg.data, returnCode = _arg.returnCode;
+          if (returnCode === 0) {
+            _this.auth = {
+              user_id: parseInt(user.user_id),
+              permission: 'read_write',
+              key: user.read_write_key
+            };
+            $.cookie('auth', _this.auth);
+          }
+          return cb();
+        };
+      })(this));
+    };
+
+    App.prototype.logout = function() {
+      this.auth = null;
+      return $.removeCookie('auth');
+    };
+
+    App.prototype.selectPage = function(page) {
+      $('.page').addClass('page-hidden');
+      return $(page).removeClass('page-hidden');
+    };
+
+    App.prototype.getGames = function(cb) {
+      if (cb == null) {
+        cb = (function() {});
+      }
+      return this.callAris('games.getGamesForUser', {}, (function(_this) {
+        return function(_arg) {
+          _this.games = _arg.data;
+          return cb();
+        };
+      })(this));
+    };
+
+    return App;
+
+  })();
+
+  app = new App;
+
+  window.app = app;
 
 }).call(this);
--- js/main.coffee
+++ js/main.coffee
@@ -2,59 +2,69 @@ console.log 'CoffeeScript loaded.'
 
 $.cookie.json = true
 
-# Calls a function on the Aris v2 API. For debugging purposes,
-# if no callback is given, the JSON result is printed and saved in arisResult.
-callAris = (func, json, cb = (x) -> window.arisResult = x; console.log x) ->
-  req = new XMLHttpRequest
-  req.onreadystatechange = ->
-    if req.readyState is 4
-      if req.status is 200
-        cb JSON.parse req.responseText
+class App
+  constructor: ->
+    $(document).ready =>
+
+      $('#form-login').submit =>
+        @login $('#text-username').val(), $('#text-password').val(), =>
+          if @auth?
+            @selectPage '#page-list'
+        false
+      $('#button-logout').click =>
+        @logout()
+        @selectPage '#page-login'
+
+      @loadLogin()
+      if @auth?
+        @selectPage '#page-list'
       else
-        cb false
-  req.open 'POST', "http://dev.arisgames.org/server/json.php/v2.#{func}", true
-  req.setRequestHeader 'Content-Type', 'application/x-www-form-urlencoded'
-  req.send JSON.stringify json
+        @selectPage '#page-login'
 
-loadLogin = ->
-  window.auth = $.cookie 'auth'
+  # Calls a function from the Aris v2 API. For debugging purposes,
+  # if no callback is given, the JSON result is logged and saved in @arisResult.
+  callAris: (func, json, cb = (x) -> @arisResult = x; console.log x) ->
+    if @auth?
+      json.auth = @auth
+    req = new XMLHttpRequest
+    req.onreadystatechange = =>
+      if req.readyState is 4
+        if req.status is 200
+          cb JSON.parse req.responseText
+        else
+          cb false
+    req.open 'POST', "http://dev.arisgames.org/server/json.php/v2.#{func}", true
+    req.setRequestHeader 'Content-Type', 'application/x-www-form-urlencoded'
+    req.send JSON.stringify json
 
-login = (username, password, cb = (->)) ->
-  callAris 'users.logIn',
-    user_name: username
-    password: password
-    permission: 'read_write'
-  , ({data: user, returnCode}) ->
-    if returnCode is 0
-      window.auth =
-        user_id: parseInt user.user_id
-        permission: 'read_write'
-        key: user.read_write_key
-      $.cookie 'auth', window.auth
-    cb()
+  loadLogin: ->
+    @auth = $.cookie 'auth'
 
-logout = ->
-  window.auth = null
-  $.removeCookie 'auth'
+  login: (username, password, cb = (->)) ->
+    @callAris 'users.logIn',
+      user_name: username
+      password: password
+      permission: 'read_write'
+    , ({data: user, returnCode}) =>
+      if returnCode is 0
+        @auth =
+          user_id:    parseInt user.user_id
+          permission: 'read_write'
+          key:        user.read_write_key
+        $.cookie 'auth', @auth
+      cb()
 
-selectPage = (page) ->
-  $('.page').addClass 'page-hidden'
-  $(page).removeClass 'page-hidden'
+  logout: ->
+    @auth = null
+    $.removeCookie 'auth'
 
-$(document).ready ->
+  selectPage: (page) ->
+    $('.page').addClass 'page-hidden'
+    $(page).removeClass 'page-hidden'
 
-  $('#button-login').click ->
-    login $('#text-username').val(), $('#text-password').val(), ->
-      if window.auth?
-        selectPage '#page-list'
-  $('#button-logout').click ->
-    logout()
-    selectPage '#page-login'
+  getGames: (cb = (->)) ->
+    @callAris 'games.getGamesForUser', {}, ({data: @games}) =>
+      cb()
 
-  loadLogin()
-  if window.auth?
-    selectPage '#page-list'
-  else
-    selectPage '#page-login'
-
-window.callAris = callAris
+app = new App
+window.app = app
