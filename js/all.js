bd4c0b5a082e3f46dda3411027cedc0df43d53e5
Michael Tolly <miketolly@gmail.com>
2015-03-04 15:38:24 -0600

    starting to build the front page

--- js/all.js
+++ js/all.js
@@ -6,12 +6,25 @@
       $(document).ready((function(_this) {
         return function() {
           _this.aris = new Aris;
+          $('#menu-logout').click(function() {
+            _this.aris.logout();
+            return _this.updateNav();
+          });
           return _this.aris.login(void 0, void 0, function() {
-            return $('body').text(JSON.stringify(_this.aris));
+            return _this.updateNav();
           });
         };
       })(this));
     }
+
+    App.prototype.updateNav = function() {
+      if (this.aris.auth != null) {
+        $('#span-username').text(this.aris.auth.username);
+        return $('#dropdown-logged-in').show();
+      } else {
+        return $('#dropdown-logged-in').hide();
+      }
+    };
 
     return App;
 
--- js/main.coffee
+++ js/main.coffee
@@ -2,7 +2,19 @@ class App
   constructor: ->
     $(document).ready =>
       @aris = new Aris
+
+      $('#menu-logout').click =>
+        @aris.logout()
+        @updateNav()
+
       @aris.login undefined, undefined, =>
-        $('body').text JSON.stringify @aris
+        @updateNav()
+
+  updateNav: ->
+    if @aris.auth?
+      $('#span-username').text @aris.auth.username
+      $('#dropdown-logged-in').show()
+    else
+      $('#dropdown-logged-in').hide()
 
 window.app = new App
