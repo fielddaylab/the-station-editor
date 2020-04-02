e4ed34ca379e298478b5f01d505889618314bffd
Michael Tolly <miketolly@gmail.com>
2020-04-02 13:22:15 -0500

    Update dependencies; remove gulp (run webpack+sass directly)

---
+++ web/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = [{
+  mode: 'production',
+  entry: './editor-react/js/main.js',
+  output: {
+    path: path.resolve(__dirname, "editor-react"),
+    filename: 'webpack_out.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+}, {
+  mode: 'production',
+  entry: './discover/js/main.js',
+  output: {
+    path: path.resolve(__dirname, "discover"),
+    filename: 'webpack_out.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+}];
