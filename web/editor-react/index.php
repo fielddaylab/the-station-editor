e94ac1738374faa51190d3ecb26e0bea6504b65a
Michael Tolly <miketolly@gmail.com>
2020-04-22 14:09:53 -0500

    Replace siftr page title and favicon

--- web/editor-react/index.php
+++ web/editor-react/index.php
@@ -1,16 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Siftr Editor</title>
+<title>STEMports Editor</title>
 <link rel="apple-touch-icon" sizes="180x180" href="../assets/favicon/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="../assets/favicon/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="../assets/favicon/favicon-16x16.png">
-<link rel="manifest" href="../assets/favicon/manifest.json">
-<link rel="mask-icon" href="../assets/favicon/safari-pinned-tab.svg" color="#5bbad5">
-<meta name="theme-color" content="#ffffff">
-<meta name="msapplication-TileColor" content="#da532c">
-<meta name="msapplication-TileImage" content="../assets/favicon/mstile-150x150.png">
-<meta name="theme-color" content="#ffffff">
+<link rel="manifest" href="../assets/favicon/site.webmanifest">
 <meta charset="UTF-8">
 <meta content="True" name="HandheldFriendly">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
