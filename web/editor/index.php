25c6dce42813a388ef00c5b850a0e285a093dc44
David Gagnon <djgagnon@wisc.edu>
2022-03-05 00:47:25 -0600

    Name page "Station Editor"

--- web/editor/index.php
+++ web/editor/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>STEMports Editor</title>
+<title>Station Editor</title>
 <link rel="apple-touch-icon" sizes="57x57" href="../assets/favicon/apple-icon-57x57.png">
 <link rel="apple-touch-icon" sizes="60x60" href="../assets/favicon/apple-icon-60x60.png">
 <link rel="apple-touch-icon" sizes="72x72" href="../assets/favicon/apple-icon-72x72.png">
