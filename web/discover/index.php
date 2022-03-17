fe6e97dec3ff36357d0481029f6df507e9140cc5
David Gagnon <djgagnon@wisc.edu>
2022-03-16 21:21:01 -0500

    Redirect main page to mmsa.org

--- web/discover/index.php
+++
@@ -1,7 +0,0 @@
-<?php $path = $_SERVER['DOCUMENT_ROOT']; $path .= "/includes/header.php";  include_once($path); ?>
-<?php $path = $_SERVER['DOCUMENT_ROOT']; $path .= "/includes/nav.php";  include_once($path); ?>
-
-<script type="text/javascript" src="webpack_out.js?cb=20180831"></script>
-<div id="the-container" class="dark_bg"></div>
-
-<?php $path = $_SERVER['DOCUMENT_ROOT']; $path .= "/includes/footer.php";  include_once($path); ?>
