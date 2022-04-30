d4a457eda9ef45f0a54b3ab02115ccdf359812d1
David Gagnon <djgagnon@wisc.edu>
2022-04-30 08:34:09 -0500

    Remove discover page (from siftr)

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -18,22 +18,4 @@ module.exports = [{
       }
     ]
   }
-}, {
-  mode: 'development',
-  entry: './discover/js/main.js',
-  output: {
-    path: path.resolve(__dirname, "discover"),
-    filename: 'webpack_out.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  }
 }];
